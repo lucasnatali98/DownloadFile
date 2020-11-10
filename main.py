890c683cb1d0faedb74c72a9c0c3ce9e60ccaddf
lucasnatali98 <lucasnatali16@gmail.com>
2020-11-10 09:39:06 -0300

    refactor variable names

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import os
 import requests
 
 
-def baixar_arquivo(url, endereco):
+def download_file(url, path):
     # faz requisição ao servidor
-    resposta = requests.get(url)
-    if resposta.status_code == requests.codes.OK:
-        with open(endereco, 'wb') as novo_arquivo:
-            novo_arquivo.write(resposta.content)
-        print("Donwload finalizado. Salvo em: {}".format(endereco))
+    response = requests.get(url)
+    if response.status_code == requests.codes.OK:
+        with open(path, 'wb') as new_file:
+            new_file.write(response.content)
+        print("Download finalizado. Salvo em: {}".format(path))
     else:
-        resposta.raise_for_status()
+        response.raise_for_status()
 
 if __name__ == "__main__":
     BASE_URL = 'https://math.mit.edu/classes/18.745/Notes/Lecture_{}_Notes.pdf'
+
     OUTPUT_DIR = 'output'
     for i in range(1, 26):
-        nome_arquivo = os.path.join(OUTPUT_DIR, 'nota_de_aula_{}.pdf'.format(i))
-        baixar_arquivo(BASE_URL.format(i), nome_arquivo)
+        name_file = os.path.join(OUTPUT_DIR, 'nota_de_aula_{}.pdf'.format(i))
+        download_file(BASE_URL.format(i), name_file)
